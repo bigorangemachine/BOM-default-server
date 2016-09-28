920e55cb215f2201e2bf7c1af0b86e910cedd8a0
Brian Dinga <brian.dinga@telus.com>
2016-09-28 13:21:14 -0400

    Setup for server sample

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
-var express=require('express');
-var app=express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
+//modules
+var _ = require('underscore'),//http://underscorejs.org/
+    merge = require('merge'),//allows deep merge of objects
+    fs = require('fs'),
+    url = require('url'),
+    utils = require('bom-utils'),
+    vars = require('bom-utils/vars');
+//custom modules - for WIP
+//     vars = require('./jspkg/vars');
+//varaibles
+var doc_root='',
+    root_params={
+        'silent':false,//actual settings
+        'rootmodule':'',
+        'config':'./config',
+        'found_params':[]
+    };
+root_params.config=root_params.config;
+var config=require('./jspkg/configurator')(process, fs, root_params);
+doc_root=root_params.doc_root;
+var express=require('express'),
+    app=express();
+
+app.get('*', function (req, res, next) {
+    var req_file=utils.check_strip_first(url.parse(req.url).pathname,'/');//clean off any query strings
+    fs.stat(doc_root+'www-assets/'+req_file, function(err, stats){
+        if(err || !stats.isFile()){
+            res.sendFile(doc_root+'www-assets/index.html');
+            //next(); <- not needed right now
+        }else{
+            res.sendFile(doc_root+'www-assets/'+req_file, 'binary');
+            //next(); <- not needed right now
+        }
+    });
+
+
 });
 
+var listeners={'80':{},'443':{},'3000':{}},
+    standup_port=function(portNo){
+        return function(err,boolRes){
+            if(!err || err===null){
+                listeners[portNo.toString()]=app.listen(portNo, function(){
+                    var address=listeners[portNo.toString()].address(),
+                        host=listeners[portNo.toString()].address().address,
+                        port=listeners[portNo.toString()].address().port;
 
-var server80=app.listen(80, function () {
-    var host=server8081.address().address,
-        port=server8081.address().port;
-
-   console.log("Example app listening at http://%s", host)
-});
-
-
-var server443=app.listen(443, function () {
-    var host=server8081.address().address,
-        port=server8081.address().port;
-
-    console.log("Example app listening at https://%s:%s", host, port)
-});
-
-var server666=app.listen(666, function () {
-    var host=server666.address().address,
-        port=server666.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port)
-});
+                    console.log("Example app listening on port '"+portNo+"' ",address);
+                });
+            }else{
+console.log("ERR: ",err);
+            }
+        };
+    };
+var isPortTaken=function(port, fn) {
+  var net=require('net'),tester=net.createServer();
+  tester.once('error', function(err){if(err.code!='EADDRINUSE'){return fn(err);};fn(null, true);}).once('listening', function(){tester.once('close', function(){fn(null, false);}).close();}).listen(port);
+};
+isPortTaken(80,standup_port(80));
+isPortTaken(443,standup_port(443));
+isPortTaken(3000,standup_port(3000));
