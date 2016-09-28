89724a7e131df5866bc7c599d9c449a903ce2352
Brian Dinga <brian.dinga@telus.com>
2016-09-28 18:01:17 -0400

    Change

--- index.js
+++ index.js
@@ -6,24 +6,14 @@ app.get('/', function (req, res) {
 });
 
 
-var server80=app.listen(80, function () {
-    var host=server8081.address().address,
-        port=server8081.address().port;
+var server3000=app.listen(3000, function () {
+    var address=server3000.address();
 
-   console.log("Example app listening at http://%s", host)
+   console.log("Example app listening Port 3000\n", address);
 });
 
+var server9000=app.listen(9000, function () {
+    var address=server9000.address();
 
-var server443=app.listen(443, function () {
-    var host=server8081.address().address,
-        port=server8081.address().port;
-
-    console.log("Example app listening at https://%s:%s", host, port)
-});
-
-var server666=app.listen(666, function () {
-    var host=server666.address().address,
-        port=server666.address().port;
-
-    console.log("Example app listening at http://%s:%s", host, port)
+   console.log("Example app listening Port 9000\n", address);
 });
