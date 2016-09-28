7534ce33e853d230770d57b397a6bf9661247aaf
Brian Dinga <brian.dinga@telus.com>
2016-09-28 18:17:33 -0400

    completed step 2 example

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+var _ = require('underscore'),//http://underscorejs.org/
+    merge = require('merge'),//allows deep merge of objects
+    fs = require('fs'),
+    url = require('url'),
+    utils = require('bom-utils'),
+    vars = require('bom-utils/vars');
+
 var express=require('express');
 var app=express();
 
-app.get('/', function (req, res) {
-   res.send('Hello World');
-});
+var doc_root='/projects/node-default-server/';doc_root='/Users/McOrange/localhost/www/node-default-server/';
+
+
+
 
 
 var server3000=app.listen(3000, function () {
@@ -16,4 +24,10 @@ var server9000=app.listen(9000, function () {
     var address=server9000.address();
 
    console.log("Example app listening Port 9000\n", address);
+});
+
+
+
+app.get('*', function (req, res, next) {
+    res.sendFile(doc_root+'www-assets/index.html');
 });
